a841ed9ea32ad219874b1901258665576bebce0f
Hackbright Student <user@mini-002.(none)>
2013-10-28 17:48:56 -0700

    Added SQLalchemy relationships

--- model.py
+++ model.py
@@ -1,13 +1,58 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    gender = Column(String, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64))
+    released_at = Column(Date())
+    imdb_url = Column(String(64), nullable=True)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer, ForeignKey('movies.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    rating = Column(Integer)
+
+    user = relationship("User", backref=backref("ratings", order_by=id))
+    movie = relationship("Movie", backref=backref("ratings", order_by=id))
 
 
 ### End class declarations
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo = True)
+    Session = sessionmaker(bind=ENGINE)
+    Base.metadata.create_all(ENGINE)
+
+
+    return Session()
+
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,22 +1,45 @@
 import model
 import csv
+import datetime
+
 
 def load_users(session):
     # use u.user
-    pass
+    with open('seed_data/u.user') as csvfile:
+        user_info = csv.reader(csvfile, delimiter='|')
+        for row in user_info:
+            user = model.User(id=row[0], age=row[1], gender=row[2], zipcode=row[4])
+            session.add(user)
+        session.commit()
 
 def load_movies(session):
     # use u.item
-    pass
+    with open('seed_data/u.item') as csvfile:
+        movie_info = csv.reader(csvfile, delimiter='|')
+        for row in movie_info:
+            release_date = datetime.datetime.strptime(row[2], "%d-%b-%Y")
+            title=row[1].decode('latin-1')
+            title=title.split(" (")
+            title = title[0]
+            movie = model.Movie(id=row[0], name=title, released_at=release_date, imdb_url=row[4])
+            session.add(movie)
+        session.commit()
 
 def load_ratings(session):
     # use u.data
-    pass
+    with open('seed_data/u.data') as csvfile:
+        rating_info = csv.reader(csvfile, delimiter='\t')
+        for row in rating_info:
+            rating = model.Rating(movie_id=row[0], user_id=row[1], rating=row[2])
+            session.add(rating)
+        session.commit()
+
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
     pass
-
+    
 if __name__ == "__main__":
     s= model.connect()
     main(s)
